46feaef999e453e9d7927f3a7496710f456b18b0
poximy <zv.erick@protonmail.com>
2021-03-03 14:23:52 -0600

    feat(mongo): Add get url function

--- data/mongo.py
+++ data/mongo.py
@@ -5,3 +5,9 @@ async def get_uri():
     async with aiofiles.open("uri.txt", mode='r', encoding='utf-8') as file:
         uri = await file.readline()
         return uri.rstrip("\n")
+
+
+async def get_url(collection, url_id) -> dict:
+    find = {'_id': url_id}
+    result: dict = await collection.find_one(find)
+    return result
