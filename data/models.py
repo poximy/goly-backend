5a510f1851281fe7ac3372b922f1f64f83577ad9
Erick ZV <zv.erick@protonmail.com>
2021-06-16 07:47:45 -0500

    Create metadata model

--- data/models.py
+++ data/models.py
@@ -3,3 +3,9 @@ from pydantic import BaseModel, Field
 
 class Url_ID(BaseModel):
     id: str = Field(alias='_id')
+
+
+class Url_Metadata(BaseModel):
+    id: str = Field(alias="_id")
+    created: str
+    clicks: int
