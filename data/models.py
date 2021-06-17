59db3aa15c157a7e9fbc9b5738a39dc800da88eb
Erick ZV <zv.erick@protonmail.com>
2021-06-16 21:53:15 -0500

    Add User model

--- data/models.py
+++ data/models.py
@@ -1,8 +1,16 @@
+from typing import Set
+
 from pydantic import BaseModel, Field
 
 
 class Url_ID(BaseModel):
     id: str = Field(alias='_id')
+
+
+class User(BaseModel):
+    user_name: str
+    password: str
+    urls: Set[Url_ID]
 
 
 class Url_Metadata(BaseModel):
