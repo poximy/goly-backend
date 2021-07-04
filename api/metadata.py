06907d1d36a7c6e271e8a33a029a672ee419f6a6
Erick ZV <zv.erick@protonmail.com>
2021-07-04 12:47:14 -0500

    Add auth to metadata

--- api/metadata.py
+++ api/metadata.py
@@ -1,15 +1,25 @@
 from typing import List
 
+import jwt
 from data import models
 from data.mongo import UrlDB
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends, HTTPException, Request, status
+from fastapi.security import OAuth2PasswordBearer
 
 router = APIRouter()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
 
 @router.get("/user/{user_name}", response_model=List[models.UrlMetadata])
-async def metadata(request: Request, user_name: str):
-    database: UrlDB = request.state.db
-    user_urls = await database.get_user_urls("user", user_name)
-    data = await database.get_metadata("metadata", user_urls)
-    return data
+async def metadata(request: Request, user_name: str, token: str = Depends(oauth2_scheme)):
+    jwt_data = jwt.decode(token, "JWT_SECRET", algorithms=["HS256"])
+    if user_name == jwt_data["user"]:
+        database: UrlDB = request.state.db
+        user_urls = await database.get_user_urls("user", user_name)
+        data = await database.get_metadata("metadata", user_urls)
+        return data
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Cannot access data from other accounts",
+    )
