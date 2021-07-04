6af35007bed63f156830e688ef0b9b391cbbfcd4
Erick ZV <zv.erick@protonmail.com>
2021-07-04 12:22:03 -0500

    Add auth token path

---
+++ api/auth.py
@@ -0,0 +1,24 @@
+import jwt
+from data import models, mongo
+from fastapi import APIRouter, Depends, HTTPException, Request, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+router = APIRouter(tags=["auth"])
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@router.post("/token")
+async def token_gen(request: Request, form_data: OAuth2PasswordRequestForm = Depends()):
+    database: mongo.UrlDB = request.state.db
+    data = {"user_name": form_data.username, "password": form_data.password}
+    user = models.User(**data)
+    valid = await database.user_login("user", user)
+    if valid:
+        token_data = {"user": form_data.username}
+        token = jwt.encode(token_data, "JWT_SECRET")
+        return {"access_token": token}
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect user name or password",
+    )
