46172cc812a100e59a3f80cfe528531ce96f4562
Erick ZV <zv.erick@protonmail.com>
2021-07-18 23:45:45 -0500

    Add url test

---
+++ tests/api/test_url.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+from src.main import app
+
+client = TestClient(app)
+
+
+def test_post_get_url():
+    url = "https://google.com"
+
+    post_response = client.post("/", json={"url": url})
+    post_json = post_response.json()
+
+    assert post_response.status_code == 201
+    assert len(post_json["_id"]) == 5
+
+    get_response = client.get("/" + post_json["_id"])
+    assert get_response.status_code != 404
