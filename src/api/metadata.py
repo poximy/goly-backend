b0bf629fb0f536e6ec34d482070c6c64cdedae63
Erick ZV <zv.erick@protonmail.com>
2021-07-24 23:30:55 -0500

    Fix metadata

--- src/api/metadata.py
+++ src/api/metadata.py
@@ -11,17 +11,11 @@ router = APIRouter(tags=["metadata"])
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-@router.get("/user/{user_name}", response_model=List[models.UrlMetadata])
-async def metadata(
-    request: Request, user_name: str, token: str = Depends(oauth2_scheme)
-):
+@router.get("/user/metadata", response_model=List[models.UrlMetadata])
+async def metadata(request: Request, token: str = Depends(oauth2_scheme)):
     jwt_data = jwt.decode(token, request.state.jwt, algorithms=["HS256"])
-    if user_name == jwt_data["user"]:
-        database: UrlDB = request.state.db
-        user_urls = await database.get_user_urls("user", user_name)
-        data = await database.get_metadata("metadata", user_urls)
-        return data
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Cannot access data from other accounts",
-    )
+    database: UrlDB = request.state.db
+
+    user_urls = await database.get_user_urls("user", jwt_data["user"])
+    data = await database.get_metadata("metadata", user_urls)
+    return data
