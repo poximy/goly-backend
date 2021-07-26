3b6e8f2d8adc7fe6ef40d69b698420560b8d24be
Erick ZV <zv.erick@protonmail.com>
2021-07-25 20:34:08 -0500

    Remove redundant imports

--- src/api/metadata.py
+++ src/api/metadata.py
@@ -1,7 +1,7 @@
 from typing import List
 
 import jwt
-from fastapi import APIRouter, Depends, HTTPException, Request, status
+from fastapi import APIRouter, Depends, Request
 from fastapi.security import OAuth2PasswordBearer
 from src.data import models
 from src.data.mongo import UrlDB
