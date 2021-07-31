102cf4ef95d6ccd31475a1c5b44bee2f5477df34
Erick ZV <zv.erick@protonmail.com>
2021-07-31 15:22:04 -0500

    Rearrange lines for better readability

--- src/api/metadata.py
+++ src/api/metadata.py
@@ -14,9 +14,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @router.get("/user/metadata", response_model=List[models.UrlMetadata])
 async def metadata(request: Request, token: str = Depends(oauth2_scheme)):
-    jwt_data = jwt.decode(token, request.state.jwt, algorithms=["HS256"])
     database: UrlDB = request.state.db
 
+    jwt_data = jwt.decode(token, request.state.jwt, algorithms=["HS256"])
     user_urls = await database.get_user_urls("user", jwt_data["user"])
     data = await database.get_metadata("metadata", user_urls)
     return data
