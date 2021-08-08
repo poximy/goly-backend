0b129f1f85978b539c4e9e6a8c3e95adb5e01c9c
Erick ZV <zv.erick@protonmail.com>
2021-08-08 16:04:00 -0500

    Fix test

--- tests/api/test_url.py
+++ tests/api/test_url.py
@@ -12,7 +12,7 @@ def test_post_get_url():
     post_json = post_response.json()
 
     assert post_response.status_code == 201
-    assert len(post_json["_id"]) == 5
+    assert len(post_json["_id"]) == 6
 
     get_response = client.get("/" + post_json["_id"])
     assert get_response.status_code != 404
